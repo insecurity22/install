09fc4d0a55870b67af156c9db28fa60c419197e9
insecurity22 <insecurity22@naver.com>
2022-05-16 16:48:35 +0900

    Update react-native-cli.sh

--- react-native/react-native-cli.sh
+++ react-native/react-native-cli.sh
@@ -1,39 +1,39 @@
-echo "Install homebrew"
+echo "\n\nInstall homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew --version
 
-echo "Install Nodejs"
+echo "\n\nInstall Nodejs"
 brew install node
 node -–version
 npm --version
 
-echo "Install watchman"
+echo "\n\nInstall watchman"
 xcode-select --install
 brew install watchman
 watchman --version
 
-echo "Install react-native-cli"
+echo "\n\nInstall react-native-cli"
 npm install -g npm@8.9.0
 npm install -g react-native-cli
 npx react-native --version
 
-echo "After installing Xcode from Appstore, enter (y|Y)."
+echo "\n\nAfter installing Xcode from Appstore, enter (y|Y)."
 read answer
 if [ ${answer} == 'y' ] || [ ${answer} == 'Y' ] 
   then
   else exit 0
 fi
 
-echo "Install Cocoapods"
+echo "\n\nInstall Cocoapods"
 sudo gem install cocoapods
 pod --version
 
-echo "Install jdk"
+echo "\n\nInstall jdk"
 brew tap AdoptOpenJDK/openjdk
 brew install --cask adoptopenjdk8
 java -version
 
-echo "After installing Android studio, enter (y|Y)."
+echo "\n\nAfter installing Android studio, enter (y|Y)."
 echo "https://developer.android.com/studio"
 read answer
 if [ ${answer} == 'y' ] || [ ${answer} == 'Y' ] 
@@ -42,7 +42,7 @@ if [ ${answer} == 'y' ] || [ ${answer} == 'Y' ]
 fi
 
 read shell
-echo "Enter (zshrc|bash_profile) to add environment variable."
+echo "\n\nEnter (zshrc|bash_profile) to add environment variable."
 echo "Enter (n|N) to skip."
 if [ ${shell} == 'zshrc' ]
 then
