c1c128c0b4d67c022782ad907ed6c6b7151b4d46
insecurity22 <insecurity22@naver.com>
2022-05-18 17:33:46 +0900

    Update react-native-app.sh

--- react-native/react-native-app.sh
+++ react-native/react-native-app.sh
@@ -7,11 +7,12 @@ cd ./${name}
 
 echo "Start (ios|android)"
 read start
-if [ $start  -eq 'ios' ];
+if [ $start -eq 'ios' ];
   then
     npm run ios
     # or react-native run-ios
-  elif [ $start -eq 'android' ]; 
+  elif [ $start -eq 'android' ];
+  then
     npm run android 
     # or react-native run-android
 fi
